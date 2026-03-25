c21b043f1a6958062a60bcef7e695c1de076f3a0
viquezdev <luisdaniel3113@hotmail.com>
2026-03-25 12:30:54 -0600

    Ejercicios requests  version final

--- FRONTEND/JAVASCRIPT_FRONTEND/Requests/Requests-DOM/js/signup.js
+++ FRONTEND/JAVASCRIPT_FRONTEND/Requests/Requests-DOM/js/signup.js
@@ -2,21 +2,30 @@ const form = document.querySelector(".register-form");
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
+
   const userId = Date.now();
+
   const name = document.querySelector("#full_name").value;
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
   const address = document.querySelector("#address").value;
 
   try {
-    const userData = await createUser(name, email, password, address);
+    await createUser(name, email, password, address);
 
-    alert("Usuario creado correctamente! Tu id es " + userData.id);
+    const user = {
+      id: userId,
+      name: name,
+      email: email,
+      password: password,
+      address: address,
+    };
 
-    localStorage.setItem(
-      "user",
-      JSON.stringify({ id: userId, ...userData.json }),
-    );
+    alert("Usuario creado correctamente! Tu id es " + userId);
+
+    localStorage.setItem("registeredUser", JSON.stringify(user));
+
+    localStorage.setItem("sessionUser", JSON.stringify(user));
 
     window.location.href = "profile.html";
   } catch (error) {
@@ -36,9 +45,8 @@ async function createUser(name, email, password, address) {
     };
 
     const response = await axios.post(url, userData);
-    console.log(response.status);
-    const data = await response.data;
-    return data;
+
+    return response.data;
   } catch (error) {
     console.log("error creating user");
     throw error;
