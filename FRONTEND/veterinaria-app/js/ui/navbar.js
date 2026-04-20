e7d4d5bddc7793ffb0e3cf7728f57e97d2fe1dd6
viquezdev <luisdaniel3113@hotmail.com>
2026-04-20 13:00:07 -0600

    veterinaria app logout

--- FRONTEND/veterinaria-app/js/ui/navbar.js
+++ FRONTEND/veterinaria-app/js/ui/navbar.js
@@ -1,36 +1,43 @@
-import { getSession } from "../services/sessionService.js";
+import { getSession, clearSession } from "../services/sessionService.js";
 
+export function setupNavbar() {
+  const loginBtn = document.querySelector("#btnLogin");
+  const logoutBtn = document.querySelector("#btnLogout");
+  const session = getSession();
 
-export function setupNavbar(){
-
-const loginBtn = document.querySelector("#btnLogin");
-
-  if(loginBtn){
+  if (loginBtn) {
     loginBtn.addEventListener("click", () => {
       window.location.href = "login.html";
     });
   }
 
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+      const salir = confirm(
+        "¿Deseas cerrar sesión? Tendrás que iniciar sesión nuevamente.",
+      );
+      if (salir) {
+        clearSession();
+        window.location.href = "login.html";
+      }
+    });
+  }
 }
 
-
-export function renderNavbar(){
+export function renderNavbar() {
   const session = getSession();
   const loginBtn = document.querySelector("#btnLogin");
   const userInfo = document.querySelector("#userInfo");
   const logoutBtn = document.querySelector("#btnLogout");
-  
 
-  if(session){
-   loginBtn.classList.add("hidden");
-   userInfo.textContent ="Usuario: " + session.user.username;
-   userInfo.classList.remove("hidden");
-   logoutBtn.classList.remove("hidden");
-}
-else{
-   loginBtn.classList.remove("hidden");
-   userInfo.classList.add("hidden");
-   logoutBtn.classList.add("hidden");
-}
-
+  if (session) {
+    loginBtn.classList.add("hidden");
+    userInfo.textContent = "Usuario: " + session.user.username;
+    userInfo.classList.remove("hidden");
+    logoutBtn.classList.remove("hidden");
+  } else {
+    loginBtn.classList.remove("hidden");
+    userInfo.classList.add("hidden");
+    logoutBtn.classList.add("hidden");
+  }
 }
