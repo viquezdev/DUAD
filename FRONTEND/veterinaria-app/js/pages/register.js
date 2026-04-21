a0cb8e4ea1e650faed9f8c98327ac59663f12201
viquezdev <luisdaniel3113@hotmail.com>
2026-04-21 14:57:39 -0600

    veterinaria app register

--- FRONTEND/veterinaria-app/js/pages/register.js
+++ FRONTEND/veterinaria-app/js/pages/register.js
@@ -1,3 +1,55 @@
 import { setupNavbar } from "../ui/navbar.js";
 
 setupNavbar();
+
+const form = document.querySelector(".login-form");
+const message = document.querySelector(".error-message");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value;
+  const verifyPassword = document.querySelector("#verify-password").value;
+
+  if (password !== verifyPassword) {
+    message.textContent = "Verifica que ambas contraseñas sean iguales";
+    return;
+  }
+
+  message.textContent = "";
+
+  registerUser(username, email, password);
+});
+
+async function registerUser(username, email, password) {
+  try {
+    const url = "http://localhost:5000/users/users";
+
+    const userData = {
+      username: username,
+      email: email,
+      password: password,
+      is_admin: false,
+      created_at: "",
+      updated_at: "",
+    };
+
+    const response = await axios.post(url, userData);
+
+    const data = response.data;
+
+    alert(data);
+    window.location.href = "login.html";
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+
+    if (error.response) {
+      message.textContent = error.response.data.message;
+    } else {
+      message.textContent = "Error al conectar con el servidor";
+    }
+  }
+}
